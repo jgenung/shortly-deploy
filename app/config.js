4e4709686402bd3597524a7e89e13167df15e827
Jordan Genung <jordanmgenung@gmail.com>
2015-03-05 21:21:39 -0600

    connection string modified

--- app/config.js
+++ app/config.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-var connectionString = process.env.CUSTOMCONNSTR_MONGOLAB_URI || 'mongodb://localhost/test';
+var connectionString = process.env.MONGO || 'mongodb://localhost/test';
 
 //mongoose.connect(connectionString)
 //var db = mongoose.createConnection(connectionString);
